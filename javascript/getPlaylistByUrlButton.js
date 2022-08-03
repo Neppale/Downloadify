2dd40340c52f66b455f31e2e70879a7146ca155e
Gabriel Verissimo Dias <94950993+Neppale@users.noreply.github.com>
2022-08-03 18:46:48 -0300

    feat: better responsiveness in mobile version

--- javascript/getPlaylistByUrlButton.js
+++ javascript/getPlaylistByUrlButton.js
@@ -2,12 +2,28 @@ async function getPlaylistByUrlButton() {
   clearCurrentPlaylist();
   clearPlaylistCookie();
 
+  document.getElementById("spotifyResponse").innerHTML = "";
+
   var id = document.getElementById("playlist-url").value;
   if (!id) {
     return;
   }
+
   if (id === "Neppale" || id === "neppale" || id === "NEPPALE") {
     id = "4SZEPtcRPviOADs62ZhaLp";
+    document.getElementById("playlist-url").value = id;
+  }
+
+  // If id matches the pattern of a spotify playlist url, extract only the id.
+  var match = id.match(/spotify:playlist:([a-zA-Z0-9]+)/);
+  if (match) {
+    id = match[1];
+    document.getElementById("playlist-url").value = id;
+  }
+
+  match = id.match(/open.spotify.com\/playlist\/([a-zA-Z0-9]+)/);
+  if (match) {
+    id = match[1];
     document.getElementById("playlist-url").value = id;
   }
 
