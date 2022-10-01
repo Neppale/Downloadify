220e2620b5f0a317554bd94f85cd2da1ec00e6b4
Gabriel Verissimo Dias <gabrielverissimodias@gmail.com>
2022-10-01 02:08:24 -0300

    fix: recognizes private spotify playlists

--- javascript/search/spotify/getRemotePlaylistData.js
+++ javascript/search/spotify/getRemotePlaylistData.js
@@ -2,16 +2,9 @@ async function getRemotePlaylistData(playlistId) {
   const headers = {
     Authorization: await getBearerToken(),
   };
-  var response = await fetch(
-    `https://api.spotify.com/v1/playlists/${playlistId}`,
-    {
-      headers,
-    }
-  );
-  if (response.status !== 200) {
-    response = { error: { message: "Invalid playlist Id" } };
-    return response;
-  }
+  var response = await fetch(`https://api.spotify.com/v1/playlists/${playlistId}`, {
+    headers,
+  });
   const playlistData = await response.json();
   return playlistData;
 }
